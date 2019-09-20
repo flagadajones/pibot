ba72e94e68a4263391a0884632a946089815ac6d
flagadajones <flagadajones@aliceadsl.fr>
2019-09-20 17:52:17 +0200

    visage

--- src/pibot/main.go
+++ src/pibot/main.go
@@ -2,6 +2,18 @@ package main
 
 func main() {
 	v := Visage{}
-	v.Init(&CapSize{w: 100, h: 100})
-	v.draw(&Cible{x: 0, y: 0, w: 212, h: 120})
+	v.Init(&CapSize{w: 212, h: 120})
+	/*vert*/
+	//v.draw(&Cible{x: 10, y: 80, w: 50, h: 50})
+	/*rouge*/
+	v.draw(&Cible{x: 80, y: 10, w: 50, h: 50})
+	/*vert*/
+	//v.draw(&Cible{x: 10, y: 10, w: 50, h: 50})
+	/*rouge*/
+	//v.draw(&Cible{x: 80, y: 80, w: 50, h: 50})
+
+	//v.draw(&Cible{x: 160, y: 10, w: 50, h: 50})
+	//	v.draw(&Cible{x: 0, y: 0, w: 212, h: 120})
+
+	v.run()
 }

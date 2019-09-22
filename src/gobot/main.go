1adc0ebe507131f83d5d81352cee443990ab292f
flagadajones <flagadajones@aliceadsl.fr>
2019-09-22 13:40:58 +0100

    gobot

--- src/gobot/main.go
+++ src/gobot/main.go
@@ -16,15 +16,20 @@ func main() {
 		oW.setMaxSpeed(15)
 		fmt.Print("1")
 		//Move forward one revolution
-		oW.rotate(90)
 		//if err := ow.Move(2048); err != nil {
 		//	fmt.Println(err)
 		//}
 		fmt.Print("1")
-		oW.moveTo(10, 45) //10CM 30°
-		oW.moveTo(10, -45)
-		oW.moveTo(10, 30)
-
+		oW.moveTo(20, 0) //10CM 30°
+	//oW.rotate(90)
+		oW.moveTo(20, 90) //10CM 30°
+//		oW.rotate(90)
+		oW.moveTo(20, 180) //10CM 30°
+//		oW.rotate(90)
+		oW.moveTo(20, -90) //10CM 30°
+//		oW.rotate(90)
+		
+		
 
 		//oW.moveTo(10, 30) //10CM 30°
 		//Move backward one revolution
@@ -35,7 +40,7 @@ func main() {
 
 	robot := gobot.NewRobot("stepperBot",
 		[]gobot.Connection{r},
-		[]gobot.Device{oW.stepper0/*, oW.stepper1, oW.stepper2, oW.stepper3*/},
+		[]gobot.Device{oW.stepper0, oW.stepper1, oW.stepper2, oW.stepper3},
 		work,
 	)
 

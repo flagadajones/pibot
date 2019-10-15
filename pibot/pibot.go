ee9fcfd75696ba0db44bca56c9223827f9a4cdf7
flagadajones <flagadajones@aliceadsl.fr>
2019-10-15 02:21:52 +0200

    optimize canvas

--- pibot/pibot.go
+++ pibot/pibot.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"image"
 	"log"
 	"time"
 
 	//	"github.com/faiface/pixel/pixelgl"
-=======
-	"log"
-	"time"
-
-	"github.com/faiface/pixel/pixelgl"
->>>>>>> 5640f3c... go
 	"github.com/flagadajones/pibot/facereco"
 	"github.com/flagadajones/pibot/visage"
 	"gobot.io/x/gobot"
@@ -23,10 +16,7 @@ var devices = []gobot.Device{}
 
 var robot *gobot.Robot
 var facerec *facereco.FaceRecognizer
-<<<<<<< HEAD
 var faces *[]image.Rectangle
-=======
->>>>>>> 5640f3c... go
 
 func run() {
 	log.Print("eee")
@@ -54,11 +44,7 @@ func main() {
 	v.Init(&visage.CapSize{W: 1280, H: 720})
 	work := func() {
 		//facerec.Run()
-<<<<<<< HEAD
 		//v.Run()
-=======
-		v.Run()
->>>>>>> 5640f3c... go
 
 		//	oW.moveTo(20, 0) //10CM 30°
 		//	oW.moveTo(20, 90) //10CM 30°
@@ -74,7 +60,6 @@ func main() {
 	gobot.Every(3000*time.Millisecond, func() {
 		v.Blink()
 	})
-<<<<<<< HEAD
 	gobot.Every(1000*time.Millisecond, func() {
 		faces = facerec.GetFaces()
 	})
@@ -102,20 +87,5 @@ func main() {
 	go run()
 	v.Run()
 	//	pixelgl.Run(run)
-=======
-	gobot.Every(10*time.Millisecond, func() {
-		faces := facerec.GetFaces()
-		if faces != nil && len(*faces) > 0 {
-			log.Println("face")
-			log.Println((*faces)[0])
-			log.Println("f1", 1280-(*faces)[0].Min.X, 720-(*faces)[0].Min.Y)
-			w := (*faces)[0].Max.X - (*faces)[0].Min.X
-			h := (*faces)[0].Max.Y - (*faces)[0].Min.Y
-			v.Draw(&visage.Cible{X: 1280 - (*faces)[0].Max.X, Y: 720 - (*faces)[0].Min.Y, W: w, H: h})
-		}
-	})
-
-	pixelgl.Run(run)
->>>>>>> 5640f3c... go
 
 }
